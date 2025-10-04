6310bea515514f920bed7a3f8d73c3ddb7841cb0
Juntz87 <89695737+Juntz87@users.noreply.github.com>
2025-10-05 01:15:07 +0800

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  darkMode: 'class',
-  theme: { extend: { colors: { qehNavy: '#0B3C5D', qehBlue: '#3282B8' } } },
-  plugins: [],
-}
